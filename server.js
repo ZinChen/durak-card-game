c97d364ef8f3187f3a4145e3a509782ad2349569
Yuri Zinchenko <zinchen777@gmail.com>
2016-09-23 14:41:03 +0300

    try to add rooms page, not working

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ var express = require('express'),
     durak = require('./durak.js');
 
 app.use(express.static(path.join(__dirname,'public')));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 var server = require('http').createServer(app).listen(process.env.PORT || 8080),
     io = require('socket.io').listen(server);
+
+app.get('/game/:room', function(req, res) {
+    var room = req.params.room;
+    res.render('room', {room: room});
+});
 
 io.sockets.on('connection', function(socket) {
     durak.initGame(io, socket);
